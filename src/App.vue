4850f26b255668c57bcf53147a2debb34ebedec0
Mak Magz <marktmagcanta.dev@gmail.com>
2023-04-29 20:29:24 +0900

    build: update to latest vue create structure
    * https://github.com/vuejs/create-vue
    
    ADDED:
    - e2e test
    - unit test
    - eslint
    - prettier
    - jsx support

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style scoped>
@@ -12,9 +12,11 @@
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from "url";
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), 
+    vueJsx()
   ],
   resolve: {
-    alias: [
-      {
-        find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))
-      },
-    ]
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
-
 })
